1dfd179e0a0dc0eed9c1bead8407e471c6f02497
jltng <julie.x.tang@outlook.com>
2022-11-08 14:49:53 -0500

    changed all instance variables to private, added getters and setters

--- src/main/java/entities/Course.java
+++ src/main/java/entities/Course.java
@@ -1,34 +1,70 @@
 package entities;
 
-import java.util.List;
 import java.util.ArrayList;
 
 /**
  * entity layer
  */
 public class Course {
-    /*
+    /**
     class for Course entity with private instance variables
     uses StudentUser, InstructorUser, and Task entity classes
      */
-    public String courseName;
-    public String courseInstructor;
-    public List<String> students; // will be storing the IDs of students
-    private int numStudents;
-    public List<String> tasks;  // will be storing the tasks IDs
-    public Boolean publish;
+    private String courseName;
+    private String courseInstructor;
+    private ArrayList<String> students; // stores the IDs of students enrolled in the course
+    private ArrayList<String> tasks;  // stores the IDs of the course's tasks
+    private Boolean published;
 
     /**
-     * Creates a new Course with a course name and a list of tasks
+     * Creates a new Course with a course name, instructor, and a list of tasks
      * @param courseName the name of the course
+     * @param courseInstructor the name of the course instructor
      * @param tasks the tasks corresponding to the course
      */
-    public Course(String courseName, List<String> tasks) {
+    public Course(String courseName, String courseInstructor, ArrayList<String> tasks) {
         this.courseName = courseName;
+        this.courseInstructor = courseInstructor;
         this.tasks = tasks;
-        this.courseInstructor = "pgries";
-        this.students = new ArrayList<>(numStudents);
-        this.numStudents = 0;
-        this.publish = false;  // course creation, default set to not yet published
+        this.students = new ArrayList<String>();
+        this.published = false;  // course creation, default set to not yet published
+    }
+
+    /**
+    all getters
+     */
+    public String getCourseName() {
+        return courseName;
+    }
+    public String getCourseInstructor() {
+        return courseInstructor;
+    }
+    public ArrayList<String> getStudents() {
+        return new ArrayList<String>(this.students);
+    }
+    public ArrayList<String> getTasks() {
+        return new ArrayList<String>(this.tasks);
+    }
+    public Boolean getPublished() {
+        return published;
+    }
+
+    /**
+    all setters
+     */
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+    public void setCourseInstructor(String courseInstructor) {
+        this.courseInstructor = courseInstructor;
+    }
+    public void setStudents(ArrayList<String> students) {
+        this.students = new ArrayList<String>(students);
+    }
+    public void setTasks(ArrayList<String> tasks) {
+        this.tasks = new ArrayList<String>(tasks);
+    }
+    public void setPublished(Boolean published) {
+        this.published = published;
     }
 }
