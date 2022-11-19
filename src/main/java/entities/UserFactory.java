4887e32b62c955e3ce9cbedaf8bd75b99ee8caf8
CC-3636 <natalieaduarte@gmail.com>
2022-11-19 16:30:02 -0500

    userfactory changes

--- src/main/java/entities/UserFactory.java
+++ src/main/java/entities/UserFactory.java
@@ -1,1 +1,12 @@
+package entities;
 
+
+// Entity Layer
+
+public interface UserFactory {
+    /** Entity Layer
+     * Creates Users with the specified name and password
+     */
+
+    User create(String name, String password);
+}
