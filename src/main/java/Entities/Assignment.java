32f73b510472b177f0e79daed7c660268efd2821
CC-3636 <natalieaduarte@gmail.com>
2022-11-19 15:40:32 -0500

    Revert "Merge pull request #18 from CSC207-2022F-UofT/1-feature-2-to-do-task-creation-and-maintenance"
    This reverts commit fae3f32f4b14775e5b936bc48fca9a6e899a1f37, reversing
    changes made to ada374345290135900c21a1cfeee49a3c4839495.

--- src/main/java/Entities/Assignment.java
+++ src/main/java/Entities/Assignment.java
@@ -1,39 +1,30 @@
 package Entities;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-
-public class Assignment extends Task implements Gradable, Preparatory {
+public class Assignment extends Task implements Gradable {
     // Gradable attributes
     private double weightage = 0;
     private double gradeReceived = -1; // the grade the user receives, -1 if not yet received
-
-    // Preparatory
-    private double timeSpent = 0;
-    private double timeNeeded = 0;
-    private ArrayList<ArrayList<LocalDateTime>> prepTimeScheduled;
 
     private LocalDateTime dueDate;
 
     /**
      * Create a new Assignment task with a title and due date
      * @param title - the name of the Assignment
-     * @param id - the unique ID of the Assignment
      * @param dueDate - Assignment's due date
      */
-    public Assignment(String title, String id, LocalDateTime dueDate) {
-        super(title, id);
+    public Assignment(String title, LocalDateTime dueDate) {
+        super(title);
         this.dueDate = dueDate;
     }
     /**
      * Create a new Assignment with a title, due date and priority
      * @param title - the name of the Assignment
-     * @param id - the unique ID of the Assignment
      * @param priority - the Assignment's priority
      * @param dueDate - the Assignment's due date
      */
-    public Assignment(String title, String id, int priority, LocalDateTime dueDate) {
-        super(title, id, priority);
+    public Assignment(String title, int priority, LocalDateTime dueDate) {
+        super(title, priority);
         this.dueDate = dueDate;
     }
 
@@ -54,44 +45,11 @@ public class Assignment extends Task implements Gradable, Preparatory {
     }
 
     /**
-     * Update the amount of time the user has spent preparing
-     * @param timeSpent - the amount of time being added
+     * Set a grade goal for the Test
+     * @param goal - the grade the user would like to receive
      */
-    public void updateTimeSpent(double timeSpent) {
-        this.timeSpent += timeSpent;
-    }
+    public void setGradeGoal(double goal) {
 
-    /**
-     * Set the amount of time the user needs to prepare for
-     * @param timeNeeded - the new time
-     */
-    public void setTimeNeeded(double timeNeeded) {
-        this.timeNeeded = timeNeeded;
-    }
-
-    /**
-     * Get the amount of time the user has left to prepare (before due date)
-     * @return - the amount of time the user has remaining
-     */
-    public double getTimeLeft() {
-        // subtract current date from due date
-        return 0;
-    }
-
-    /**
-     * Schedule the required prep time for the Assignment and add to prepTimeScheduled
-     * @return - whether scheduling was successful
-     */
-    public boolean schedulePrepTime() {
-        return true;
-    }
-
-    /**
-     * Get all scheduled prep times
-     * @return - all scheduled prep time
-     */
-    public ArrayList<ArrayList<LocalDateTime>> getPrepTimeScheduled() {
-        return prepTimeScheduled;
     }
 
     /**
