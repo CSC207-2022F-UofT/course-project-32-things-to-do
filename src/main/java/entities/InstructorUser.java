0563b2d7eaeebf9f0f4f3b4553c3f4f910b57230
jltng <jjulie.tang@mail.utoronto.ca>
2022-11-21 13:46:46 -0500

    updated files to that of main's

--- src/main/java/entities/InstructorUser.java
+++ src/main/java/entities/InstructorUser.java
@@ -1,20 +1,57 @@
 package entities;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class InstructorUser extends User {
+public class InstructorUser implements User {
 
     /** Entity Layer
-     * An Instructor User */
+     * An Instructor User with a name, password, and list of courses */
 
     public InstructorUser(String name, String password) {
+        this.name = name;
+        this.password = password;
+        this.courses = new ArrayList<String>();
     }
 
-    private ArrayList<String> Courses;
+    private final String name;
+
+    private final String password;
+
+    private ArrayList<String> courses;
+
+    /**
+     * @return the name of this InstructorUser
+     */
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the password of this InstructorUser's account
+     */
+    @Override
+    public String getPass() {
+        return password;
+    }
+
+    /**
+     * @return whether the entered password is valid
+     */
+    @Override
+    public boolean checkPassword() {
+        return password != null && password.length() > 8;
+    }
 
     public ArrayList<String> getCourses() {
-        return this.Courses;
+        return this.courses;
     }
 
-    public void addCourse() {}
+    public void addCourse(String course) {
+        this.courses.add(course);
+    }
+
+    public void setCourses(ArrayList<String> c) { this.courses = c; };
+
 }
--- src/main/java/entities/StudentUser.java
+++ src/main/java/entities/StudentUser.java
@@ -1,22 +1,53 @@
 package entities;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.*;
 
-public class StudentUser extends User {
+public class StudentUser implements User {
 
     /** Entity Layer
-     * A student User */
+     * A Student with a name, password, To Do List, Task Archive, Courses, Inbox, Notifications, and
+     * desired grades.
+     */
 
     public StudentUser(String name, String password) {
         this.name = name;
+        this.password = password;
         this.toDoList = new ArrayList<String>();
         this.taskArchive = new ArrayList<String>();
         this.courses = new ArrayList<String>();
         this.inbox = new ArrayList();
         this.notifications = new ArrayList<String>();
         this.desiredGrades = new HashMap<String, Double>();
-        this.workingHours = new ArrayList<List<LocalDateTime>>();
+    }
+
+    private final String name;
+
+    private final String password;
+
+    /**
+     * @return the name of this StudentUser
+     */
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the password of this StudentUser
+     */
+    @Override
+    public String getPass() {
+        return password;
+    }
+
+    /**
+     * @return whether the entered password is valid
+     */
+    @Override
+    public boolean checkPassword() {
+        return password != null && password.length() > 8;
     }
 
     private ArrayList<String> toDoList; // string being task ids
@@ -26,11 +57,27 @@ public class StudentUser extends User {
         return this.toDoList;
     }
 
+    public void addTaskToList(String task) {
+        this.toDoList.add(task);
+    }
+
+    public void removeTaskFromList(String task) { this.toDoList.remove(task); }
+
+    public void setToDoList(ArrayList<String> l) { this.toDoList = l; }
+
     private ArrayList<String> taskArchive;
 
     public ArrayList<String> getTaskArchive() {
 
         return this.taskArchive;
+    }
+
+    public void addTaskToArchive(String task) {
+        this.taskArchive.add(task);
+    }
+
+    public void setTaskArchive(ArrayList<String> t) {
+        this.taskArchive = t;
     }
 
     private ArrayList<String> courses;
@@ -39,6 +86,12 @@ public class StudentUser extends User {
         return this.courses;
     }
 
+    public void addCourse(String course) {
+        this.toDoList.add(course);
+    }
+
+    public void setCourses(ArrayList<String> c) { this.courses = c; }
+
     private ArrayList inbox ; // once Invitation class is up, make it an ArrayList of Invitations
 
     public ArrayList getInbox() {
@@ -46,32 +99,51 @@ public class StudentUser extends User {
         return this.inbox;
     }
 
+    public void addInvitation(String invite) {
+        this.inbox.add(invite);
+    }
+
+    public void removeInvitation(String invite) {
+        this.inbox.remove(invite);
+    }
+
+    public void setInbox(ArrayList i) { this.inbox = i; }
+
     private ArrayList<String> notifications;
 
     public ArrayList<String> getNotifications() {
-
         return this.notifications;
     }
 
-    private String name;
-
-    public String getName() {
-        return this.name;
+    public void addNotification(String notification) {
+        this.notifications.add(notification);
     }
+
+    public void setNotifications(ArrayList<String> n) { this.notifications = n; }
 
     private Map<String,Double> desiredGrades; // String is course id
 
-    public Map getDesiredGrades() {
+    public Map<String, Double> getDesiredGrades() {
 
         return this.desiredGrades;
     }
 
-    public void setDesiredGrades() {}
+    public void addDesiredGrade(String course, Double grade) {
+        this.desiredGrades.put(course, grade);
+    }
 
-    private List<List<LocalDateTime>> workingHours; // each internal list contains the start and end times
+    public void setDesiredGrades(Map<String, Double> m) {
+        this.desiredGrades = m;
+    }
 
-    public List<List<LocalDateTime>> getWorkingHours() {
+    private ArrayList<LocalTime> workingHours;
+
+    public ArrayList<LocalTime> getWorkingHours() {
 
         return this.workingHours;
+    }
+
+    public void setWorkingHours(ArrayList<LocalTime> hours) {
+        this.workingHours = hours;
     }
 }
