325e333c0f51dfeb6714a88318ab8ec93064f1f4
alyson647 <alyson.roh@mail.utoronto.ca>
2022-11-21 00:33:15 -0500

    Updated screen to work and added LabelTextPanel class

--- src/main/java/screens/AvailableTimesScreen.java
+++ src/main/java/screens/AvailableTimesScreen.java
@@ -15,18 +15,15 @@ public class AvailableTimesScreen extends JFrame implements CollaborativeSchedul
 
     CollaborativeSchedulingPresenter collaborativeSchedulingPresenter;
 
-    // list of times screen with pressable buttons
-    //      when you press the button, it darkens so that it indicates that it will be scheduled
-
     public ArrayList<String> present(CollaborativeSchedulingPresenter output) {
         CollaborativeSchedulingResponseModel responseModel = output.getOutputData();
         return output.prepareAvailableTimes(responseModel).getScheduleString();
     }
 
-    JTextField task = new JTextField(15);
-
+    JTextField taskName = new JTextField(15);
     JTextField timesToSchedule = new JTextField(20);
     JButton scheduleTime = new JButton("Schedule");
+//    JPanel availableTimes = new JPanel();
 
 
     public AvailableTimesScreen(ScheduleTaskController scheduleTaskController,
@@ -37,40 +34,43 @@ public class AvailableTimesScreen extends JFrame implements CollaborativeSchedul
 
         ArrayList<String> presentThese = present(collaborativeSchedulingPresenter);
 
+
         JLabel title = new JLabel("Available Times");
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        JLabel enterTask = new JLabel("Enter CollaborativeTask");
-        enterTask.setBounds(10, 50, 300, 20);
+        LabelTextPanel taskInfo = new LabelTextPanel(new JLabel("Re-enter task name"), taskName);
 
-        JLabel chooseTime = new JLabel("Enter the times you want to schedule in the format provided" +
-                "and separated by a comma " +
-                "(e.g. 2022-03-14 2:00,2022-11-15");
-        JPanel availableTimes = new JPanel();
+        LabelTextPanel timeInfo = new LabelTextPanel(new JLabel("Enter schduled times separate by a comma"),
+                timesToSchedule);
 
-        for (String time : presentThese) {
-            JButton possibleTime = new JButton(time);
-            availableTimes.add(possibleTime);
-        }
+        JLabel time1 = new JLabel(presentThese.get(0));
+
+        JButton scheduleTime = new JButton("Schedule");
 
         scheduleTime.addActionListener(this);
 
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        this.add(title);
-        this.add(enterTask);
-        this.add(task);
-        this.add(availableTimes);
-        this.add(chooseTime);
-        this.add(timesToSchedule);
-        this.add(scheduleTime);
+
+        JPanel main = new JPanel();
+        main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
+
+        main.add(title);
+        main.add(time1);
+        main.add(taskInfo);
+        main.add(timeInfo);
+        main.add(scheduleTime);
+        this.setContentPane(main);
+
+        this.pack();
+        setVisible(true);
     }
     // React to a button click that results in evt
     public void actionPerformed(ActionEvent evt) {
-        ArrayList<String> selectedTimes = new ArrayList<>();
-        if (evt.getSource() == scheduleTime) {
-            JOptionPane.showMessageDialog(this, "Successfully inputted into calendar");
-        }
-        scheduleTaskController.addTimes(task.getText(), timesToSchedule.getText());
+        // ArrayList<String> selectedTimes = new ArrayList<>();
+//        if (evt.getSource() == scheduleTime) {
+//            JOptionPane.showMessageDialog(this, "Successfully inputted into calendar");
+//        }
+        scheduleTaskController.addTimes(taskName.getText(), timesToSchedule.getText());
     }
 
 
