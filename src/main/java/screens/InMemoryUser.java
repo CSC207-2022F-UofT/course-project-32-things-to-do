b4f5e7857503f3d9a83d0688b729d2b485d0360b
CC-3636 <natalieaduarte@gmail.com>
2022-11-21 22:47:52 -0500

    made some changes to InMemoryUser so it works for testing

--- src/main/java/screens/InMemoryUser.java
+++ src/main/java/screens/InMemoryUser.java
@@ -1,12 +1,13 @@
 package screens;
 
+import login_usecase.LoginGateway;
 import user_register_usecase.UserRegSaveRequest;
 import user_register_usecase.UserRegGateway;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class InMemoryUser implements UserRegGateway {
+public class InMemoryUser implements UserRegGateway, LoginGateway {
 
     final private Map<String, UserRegSaveRequest> users = new HashMap<>();
 
@@ -18,6 +19,16 @@ public class InMemoryUser implements UserRegGateway {
     @Override
     public boolean existsByName(String identifier) {
         return users.containsKey(identifier);
+    }
+
+    @Override
+    public String passOf(String name) {
+        return null;
+    }
+
+    @Override
+    public Map<String, UserRegSaveRequest> getAccounts() {
+        return null;
     }
 
     /**
