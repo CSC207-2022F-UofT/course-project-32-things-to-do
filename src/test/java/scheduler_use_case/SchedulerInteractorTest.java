10321c060544933d59b489c2ac9cda94f1cb5fdc
celinelumqr <celine.lum@mail.utoronto.ca>
2022-11-26 17:48:03 -0500

    initial test file for scheduling use case

---
+++ src/test/java/scheduler_use_case/SchedulerInteractorTest.java
@@ -0,0 +1,34 @@
+package scheduler_use_case;
+
+import entities.*;
+import org.junit.jupiter.api.Test;
+import schedule_conflict_use_case.ScheduleConflictPresenter;
+import screens.ScheduleConflictResponseFormatter;
+import screens.SchedulerResponseFormatter;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SchedulerInteractorTest {
+
+    @Test
+    void schedule() {
+        // Initialise the classes
+        SchedulerPresenter schedulerPresenter = new SchedulerResponseFormatter();
+        ScheduleConflictPresenter scheduleConflictPresenter = new ScheduleConflictResponseFormatter();
+        SchedulerInputBoundary interactor = new SchedulerInteractor(scheduleConflictPresenter, schedulerPresenter);
+
+        // Create the test user and task
+        Event event = new Event("testEvent", "testid", 0, LocalDateTime.of(2022, 11, 29, 14, 0),
+                LocalDateTime.of(2022, 11, 29, 15, 0), false, null);
+        StudentUser user = new StudentUser("testUser", "testPassword");
+
+        // Test the interactor
+        SchedulerRequestModel requestModel = new SchedulerRequestModel(event, user);
+        interactor.schedule(requestModel);
+
+
+    }
+
+}
