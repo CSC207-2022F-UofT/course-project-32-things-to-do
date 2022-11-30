e923f3e9d11b986476a3935cbc3e7beb070631ab
CC-3636 <natalieaduarte@gmail.com>
2022-11-30 17:20:44 -0500

    merging

--- src/main/java/entities/UserFactory.java
+++ src/main/java/entities/UserFactory.java
@@ -1,1 +1,13 @@
+package entities;
 
+
+// Entity Layer
+
+public interface UserFactory {
+    /**
+     * Creates Users with the specified name and password
+     */
+    StudentUser createStudent(String name, String password);
+
+    InstructorUser createInstructor(String name, String password);
+}
