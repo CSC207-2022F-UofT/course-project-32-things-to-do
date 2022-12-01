34446cbd168be0ad4ce597139076c1b372c56cea
yyutiw <yyutiw@gmail.com>
2022-12-01 16:49:46 -0500

    Implemented Task displayers (attempted dependency inversion for task editing)

--- src/main/java/use_cases/task_management/task_edit_use_case/EventInfoRetriever.java
+++ src/main/java/use_cases/task_management/task_edit_use_case/EventInfoRetriever.java
@@ -1,4 +1,50 @@
 package use_cases.task_management.task_edit_use_case;
 
-public class EventInfoRetriever {
+import entities.Event;
+
+import java.time.LocalDateTime;
+
+public class EventInfoRetriever implements EventDisplayer {
+    private final Event event;
+    /**
+     * An object that retrieves the info of an Event to be displayed in edit screens
+     * @param event - the Event whose info is being displayed
+     */
+    public EventInfoRetriever(Event event) {
+        this.event = event;
+    }
+    @Override
+    public String getTitle() {
+        return event.getTitle();
+    }
+
+    @Override
+    public String getId() {
+        return event.getId();
+    }
+
+    @Override
+    public int getPriority() {
+        return event.getPriority();
+    }
+
+    @Override
+    public LocalDateTime getStartTime() {
+        return event.getTimeBlock()[0];
+    }
+
+    @Override
+    public LocalDateTime getEndTime() {
+        return event.getTimeBlock()[1];
+    }
+
+    @Override
+    public boolean getRecurring() {
+        return event.getRecurring();
+    }
+
+    @Override
+    public String getFrequency() {
+        return event.getFrequency();
+    }
 }
--- src/main/java/use_cases/task_management/task_edit_use_case/TestInfoRetriever.java
+++ src/main/java/use_cases/task_management/task_edit_use_case/TestInfoRetriever.java
@@ -1,4 +1,55 @@
 package use_cases.task_management.task_edit_use_case;
 
-public class TestInfoRetriever {
+import entities.Test;
+
+import java.time.LocalDateTime;
+
+public class TestInfoRetriever implements TestDisplayer {
+    private final Test test;
+    /**
+     * An object that retrieves the info of a Test to be displayed in edit screens
+     * @param test - the Test whose info is being displayed
+     */
+    public TestInfoRetriever(Test test) {
+        this.test = test;
+    }
+    @Override
+    public String getTitle() {
+        return test.getTitle();
+    }
+
+    @Override
+    public String getId() {
+        return test.getId();
+    }
+
+    @Override
+    public int getPriority() {
+        return test.getPriority();
+    }
+
+    @Override
+    public LocalDateTime getStartTime() {
+        return test.getTimeBlock()[0];
+    }
+
+    @Override
+    public LocalDateTime getEndTime() {
+        return test.getTimeBlock()[1];
+    }
+
+    @Override
+    public double getWeightage() {
+        return test.getWeightage();
+    }
+
+    @Override
+    public double getTimeNeeded() {
+        return test.getTimeNeeded();
+    }
+
+    @Override
+    public double getTimeSpent() {
+        return test.getTimeSpent();
+    }
 }
