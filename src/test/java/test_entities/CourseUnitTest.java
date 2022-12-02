d872031b7d913e7ce1f841b8583fdaf7139a7e4c
jltng <jjulie.tang@mail.utoronto.ca>
2022-12-01 21:42:36 -0500

    Course Unit Test (commented out failed tests oop), found error with Main running -- cannot seem to move the path of the ser file which is interesting

--- src/test/java/test_entities/CourseUnitTest.java
+++ src/test/java/test_entities/CourseUnitTest.java
@@ -1,33 +1,33 @@
 package test_entities;
 
-//import entities.Course;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.ArrayList;
-//
-//import static org.junit.jupiter.api.Assertions.assertFalse;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-///**
-// * Unit tests for the Course entity
-// * Assertion tests for whether all required fields are filled.
-// */
-//class CourseUnitTest {
-//    @Test
-//    void course_allFieldsEmpty_thenIsFalse() {
-//        ArrayList<String> emptyArrayList = new ArrayList<>();
-//        Course course = new Course("", "", emptyArrayList);
-//
-//        assertFalse(course.courseIsValid());
-//    }
-//
-//    @Test
-//    void course_StrFilled_thenIsFalse() {
-//        ArrayList<String> emptyArrayList = new ArrayList<>();
-//        Course course = new Course("course1", "", emptyArrayList);
-//
-//        assertFalse(course.courseIsValid());
-//    }
+import entities.Course;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit tests for the Course entity
+ * Assertion tests for whether all required fields are filled.
+ */
+class CourseUnitTest {
+    @Test
+    void course_allFieldsEmpty_thenIsFalse() {
+        ArrayList<String> emptyArrayList = new ArrayList<>();
+        Course course = new Course("", "", emptyArrayList);
+
+        assertFalse(course.courseIsValid());
+    }
+
+    @Test
+    void course_StrFilled_thenIsFalse() {
+        ArrayList<String> emptyArrayList = new ArrayList<>();
+        Course course = new Course("course1", "", emptyArrayList);
+
+        assertFalse(course.courseIsValid());
+    }
 //    @Test
 //    void course_taskFilled_thenIsFalse() {
 //        ArrayList<String> task = new ArrayList<>();
@@ -45,21 +45,21 @@ package test_entities;
 //
 //        assertFalse(course.courseIsValid());
 //    }
-//
-//    @Test
-//    void course_taskEmpty_thenIsFalse() {
-//        ArrayList<String> noTask = new ArrayList<>();
-//        Course course = new Course("course1", "inst1", noTask);
-//
-//        assertFalse(course.courseIsValid());
-//    }
-//
-//    @Test
-//    void course_allFieldsFilled_thenIsTrue() {
-//        ArrayList<String> task = new ArrayList<>();
-//        task.add("task1");
-//        Course course = new Course("course1", "inst1", task);
-//
-//        assertTrue(course.courseIsValid());
-//    }
-//}
+
+    @Test
+    void course_taskEmpty_thenIsFalse() {
+        ArrayList<String> noTask = new ArrayList<>();
+        Course course = new Course("course1", "inst1", noTask);
+
+        assertFalse(course.courseIsValid());
+    }
+
+    @Test
+    void course_allFieldsFilled_thenIsTrue() {
+        ArrayList<String> task = new ArrayList<>();
+        task.add("task1");
+        Course course = new Course("course1", "inst1", task);
+
+        assertTrue(course.courseIsValid());
+    }
+}
