0b43dea66ceddcb44bbc4336324fa03717abfb3e
sshmuylovich <simaeshmu@gmail.com>
2022-12-07 18:35:46 -0500

    Created CollaborativeTaskEditInputBoundary

--- src/main/java/use_cases/collaborative_task_management/collaborative_task_edit_use_case/CollaborativeTaskEditInputBoundary.java
+++ src/main/java/use_cases/collaborative_task_management/collaborative_task_edit_use_case/CollaborativeTaskEditInputBoundary.java
@@ -1,5 +1,10 @@
 package use_cases.collaborative_task_management.collaborative_task_edit_use_case;
 
 public interface CollaborativeTaskEditInputBoundary {
+    /**
+     * Attempt to edit a Collaborative Task
+     * @param requestModel - the request model of the Collaborative Task being edited
+     * @return - response model
+     */
     CollaborativeTaskEditResponseModel edit(CollaborativeTaskEditRequestModel requestModel);
 }
