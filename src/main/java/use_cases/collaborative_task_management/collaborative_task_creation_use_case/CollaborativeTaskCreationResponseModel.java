65ec25c8638833d6188bb36ba75965893d847246
sshmuylovich <simaeshmu@gmail.com>
2022-12-07 18:14:13 -0500

    Created CollaborativeTaskCreationResponseModel

--- src/main/java/use_cases/collaborative_task_management/collaborative_task_creation_use_case/CollaborativeTaskCreationResponseModel.java
+++ src/main/java/use_cases/collaborative_task_management/collaborative_task_creation_use_case/CollaborativeTaskCreationResponseModel.java
@@ -1,21 +1,46 @@
 package use_cases.collaborative_task_management.collaborative_task_creation_use_case;
 
-import entities.*;
-
+import entities.StudentUser;
 import java.time.LocalDateTime;
 
-public class CollaborativeTaskCreationResponseModel {
-    final String title;
-    final LocalDateTime startTime;
-    final LocalDateTime endTime;
-    final LocalDateTime deadline;
-    final StudentUser leader;
+/**
+ * Response Model for the Collaborative Task Creation Use Case
+ * Acts as the output data object in the use case layer
+ */
 
-    public CollaborativeTaskCreationResponseModel(String title, LocalDateTime startTime, LocalDateTime endTime, LocalDateTime deadline, StudentUser leader) {
+public class CollaborativeTaskCreationResponseModel {
+    private final String title;
+    private final String id;
+    private final LocalDateTime deadline;
+    private final StudentUser leader;
+
+    /**
+     * A response model for successful Collaborative task creation.
+     * @param title - the title of the Collaborative Task
+     * @param id - the Collaborative Task's id
+     * @param deadline - the Collaborative Task's deadline
+     * @param leader - the Collaborative Task's leader
+     */
+    public CollaborativeTaskCreationResponseModel(String title, String id,  LocalDateTime deadline, StudentUser leader) {
         this.title = title;
-        this.startTime = startTime;
-        this.endTime = endTime;
+        this.id = id;
         this.deadline = deadline;
         this.leader = leader;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    public StudentUser getLeader() {
+        return leader;
     }
 }
