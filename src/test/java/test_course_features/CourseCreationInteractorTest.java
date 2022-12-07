eb08aa2e5abf0d55cbfc75902172fb42fdf0b202
jltng <jjulie.tang@mail.utoronto.ca>
2022-12-07 01:49:26 -0500

    bruh

--- src/test/java/test_course_features/CourseCreationInteractorTest.java
+++ src/test/java/test_course_features/CourseCreationInteractorTest.java
@@ -9,11 +9,42 @@ import use_cases.course_features.course_creation_use_case.*;
 import java.io.IOException;
 import java.util.ArrayList;
 
-//import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class CourseCreationInteractorTest {
+    /**
+     * testing the creation of an assignment
+     */
+    @Test
+    public void createCourse() {
+        InMemoryCourse courseGateway = new InMemoryCourse();
+
+        CourseCreationOutputBoundary outputBoundary = new CourseCreationOutputBoundary() {
+            @Override
+            public CourseCreationResponseModel prepareSuccessView(CourseCreationResponseModel newCourse) {
+                ArrayList<String> tasks = new ArrayList<String>();
+                tasks.add("task1");
+                tasks.add("task2");
+                tasks.add("task3");
+                Course course1 = new Course("course1", "inst1", tasks);
+
+                assertEquals(course1.getCourseID(), "course1inst1");
+                assertEquals(course1.getTasks(), tasks);
+                assertNotNull(course1.getTasks());
+                assertTrue(courseGateway.existsByCourseID(course1.getCourseID()));
+                return null;
+            }
+
+            @Override
+            public CourseCreationResponseModel prepareFailView(String error) {
+                fail("Use case failure is unexpected.");
+                return null;
+            }
+        };
+        CourseCreationRequestModel request = new CourseCreationRequestModel(
+                "course2", "inst2", );
+    }
     @Test
     public void create() throws IOException {
         CourseCreationDsGateway courseRepository = new InMemoryCourse();
@@ -22,7 +53,11 @@ public class CourseCreationInteractorTest {
 
             @Override
             public CourseCreationResponseModel prepareSuccessView(CourseCreationResponseModel newCourse) {
-                // 4. check output data and associated changes are correct
+                ArrayList<String> tasks = new ArrayList<String>();
+                tasks.add("task1");
+                tasks.add("task2");
+                tasks.add("task3");
+                Course course1 = new Course("course1", "inst1", tasks);
                 assertEquals("course1inst1", newCourse.getCourseID());
                 assertNotNull(newCourse.getTasks());
                 assertTrue(courseRepository.existsByCourseID("course1inst1"));
