8bdc916d497f2a4787bc7f5b0f028c8de29f08a4
sshmuylovich <simaeshmu@gmail.com>
2022-12-07 18:05:49 -0500

    Created CollaborativeTaskCreationInputBoundary

--- src/main/java/use_cases/collaborative_task_management/collaborative_task_creation_use_case/CollaborativeTaskCreationInputBoundary.java
+++ src/main/java/use_cases/collaborative_task_management/collaborative_task_creation_use_case/CollaborativeTaskCreationInputBoundary.java
@@ -1,7 +1,10 @@
 package use_cases.collaborative_task_management.collaborative_task_creation_use_case;
 
-import java.io.IOException;
-
 public interface CollaborativeTaskCreationInputBoundary {
-    CollaborativeTaskCreationResponseModel create(CollaborativeTaskCreationRequestModel requestModel) throws IOException;
+    /**
+     * Attempt to create a Collaborative Task
+     * @param requestModel - the request model for creation
+     * @return - response model
+     */
+    CollaborativeTaskCreationInputBoundary create(CollaborativeTaskCreationRequestModel requestModel);
 }
