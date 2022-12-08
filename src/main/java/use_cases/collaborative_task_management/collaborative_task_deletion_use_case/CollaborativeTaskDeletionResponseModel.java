d027862222e6474cc27988193660b93f36f7399c
sshmuylovich <simaeshmu@gmail.com>
2022-12-08 01:52:14 -0500

    Created Use Cases and Screens for CT

---
+++ src/main/java/use_cases/collaborative_task_management/collaborative_task_deletion_use_case/CollaborativeTaskDeletionResponseModel.java
@@ -0,0 +1,19 @@
+package use_cases.collaborative_task_management.collaborative_task_deletion_use_case;
+
+public class CollaborativeTaskDeletionResponseModel {
+    private final String title;
+    private final String id;
+
+    /**
+     * A response model for Collaborative Task deletion.
+     * @param title - the title of the Collaborative Task
+     * @param id - the ID of the Collaborative Task
+     */
+    public CollaborativeTaskDeletionResponseModel(String title, String id) {
+        this.title = title;
+        this.id = id;
+    }
+
+    public String getTitle() {return this.title; }
+    public String getId() { return this.id; }
+}
