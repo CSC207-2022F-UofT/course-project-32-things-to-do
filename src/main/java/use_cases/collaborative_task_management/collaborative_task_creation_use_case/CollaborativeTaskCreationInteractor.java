5f912152f04d9bb1ab1fb84299702b0e55dfd45c
sshmuylovich <simaeshmu@gmail.com>
2022-12-08 01:30:37 -0500

    Updated CT Creation Use Case

--- src/main/java/use_cases/collaborative_task_management/collaborative_task_creation_use_case/CollaborativeTaskCreationInteractor.java
+++ src/main/java/use_cases/collaborative_task_management/collaborative_task_creation_use_case/CollaborativeTaskCreationInteractor.java
@@ -1,34 +1,54 @@
 package use_cases.collaborative_task_management.collaborative_task_creation_use_case;
 
 import entities.*;
-import screens.task_management.FileTaskMap;
+import use_cases.task_management.read_write.TaskMapGateway;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
+/**
+ * Collaborative Task Creation Use Case Interactor (use case layer)
+ * Implements business logic on entities
+ */
 
 public class CollaborativeTaskCreationInteractor implements CollaborativeTaskCreationInputBoundary {
-    private final CollaborativeTaskCreationPresenter presenter;
-    private final StudentUser student;
+    private final TaskMapGateway taskMapRepository;
+    private final CollaborativeTaskCreationOutputBoundary outputBoundary;
+    private final StudentUser student = (StudentUser) CurrentUser.getCurrentUser();
 
-    public CollaborativeTaskCreationInteractor(CollaborativeTaskCreationPresenter collaborativeTaskPresenter, StudentUser student) {
-        this.presenter = collaborativeTaskPresenter;
-        this.student = student;
+    /**
+     * Interactor for Collaborative Task.
+     * @param taskMapRepository- for saving the task to a file.
+     * @param outputBoundary - the output boundary for displaying results.
+     */
+    public CollaborativeTaskCreationInteractor(TaskMapGateway taskMapRepository, CollaborativeTaskCreationOutputBoundary outputBoundary) {
+        this.taskMapRepository = taskMapRepository;
+        this.outputBoundary = outputBoundary;
     }
 
-    @Override
-    public CollaborativeTaskCreationResponseModel create(CollaborativeTaskCreationRequestModel requestModel) throws IOException {
-        if (requestModel.getTitle().equals("") || requestModel.getStartTime() == null || requestModel.getEndTime() == null || requestModel.getDeadline() == null || (requestModel.getRecurring() && requestModel.getFrequency().equals(""))) {
-            return presenter.prepareFailView("Please fill in all required information.");
-        }
+    /**
+     * Create a Collaborative Task
+     * @param requestModel - request model for Collaborative Task
+     * @return - response model after Collaborative Task is created
+     */
+    public CollaborativeTaskCreationResponseModel create(CollaborativeTaskCreationRequestModel requestModel) {
+        if (requestModel.getTitle().equals("")) return outputBoundary.prepareFailView("Please enter a title.");
+        String id = requestModel.getTitle() + "_" + student.getName() + "_collaborative";
+        if (TaskMap.findTask(id) != null) return outputBoundary.prepareFailView("Please enter a unique title.");
+        if (requestModel.getStartTime() == null) return outputBoundary.prepareFailView("Please enter a start time.");
+        if (requestModel.getEndTime() == null) return outputBoundary.prepareFailView("Please enter an end time.");
+        if (requestModel.getStartTime().isAfter(requestModel.getEndTime()))
+            return outputBoundary.prepareFailView("Please enter a valid time block.");
+        if (requestModel.getEndTime().isAfter(requestModel.getDeadline()))
+            return outputBoundary.prepareFailView("Please enter a valid deadline.");
+        if (requestModel.getDeadline() == null) return outputBoundary.prepareFailView("Please enter a deadline.");
+        if (requestModel.getRecurring() && requestModel.getFrequency().equals(""))
+            return outputBoundary.prepareFailView("Please enter a frequency.");
+        if (requestModel.getRecurring() && !(requestModel.getFrequency().equals("daily") || requestModel.getFrequency().equals("monthly") || requestModel.getFrequency().equals("weekly")))
+            return outputBoundary.prepareFailView("Please enter a valid frequency (\"daily\", \"weekly\", \"monthly\" accepted.)");
 
-        String id = LocalDateTime.now() + "_" + student.getName() + "_none";
+        CollaborativeTask newCollaborativeTask = new CollaborativeTask(requestModel.getTitle(), id, requestModel.getPriority(), requestModel.getRecurring(), requestModel.getFrequency(), requestModel.getStartTime(), requestModel.getEndTime(), requestModel.getDeadline(), student);
 
-        CollaborativeTask collaborativeTask = new CollaborativeTask(requestModel.getTitle(), id, requestModel.getPriority(), requestModel.getRecurring(), requestModel.getFrequency(), requestModel.getStartTime(), requestModel.getEndTime(), requestModel.getDeadline(), student);
-
-        FileTaskMap trw = new FileTaskMap("src/main/java/data/TaskMap");
-        trw.save(TaskMap.getTaskMap());
-
-        CollaborativeTaskCreationResponseModel collaborativeTaskResponseModel = new CollaborativeTaskCreationResponseModel(requestModel.getTitle(), requestModel.getStartTime(), requestModel.getEndTime(), requestModel.getDeadline(), requestModel.getLeader());
-        return presenter.prepareSuccessView(collaborativeTaskResponseModel);
+        TaskMap.addTask(id, newCollaborativeTask); // NEED TO MAKE SURE THIS WORKS!!
+        taskMapRepository.save(TaskMap.getTaskMap());
+        CollaborativeTaskCreationResponseModel response = new CollaborativeTaskCreationResponseModel(requestModel.getTitle(), id);
+        return outputBoundary.prepareSuccessView(response);
     }
 }
--- src/main/java/use_cases/collaborative_task_management/collaborative_task_creation_use_case/CollaborativeTaskCreationResponseModel.java
+++ src/main/java/use_cases/collaborative_task_management/collaborative_task_creation_use_case/CollaborativeTaskCreationResponseModel.java
@@ -1,8 +1,5 @@
 package use_cases.collaborative_task_management.collaborative_task_creation_use_case;
 
-import entities.StudentUser;
-import java.time.LocalDateTime;
-
 /**
  * Response Model for the Collaborative Task Creation Use Case
  * Acts as the output data object in the use case layer
@@ -11,21 +8,15 @@ import java.time.LocalDateTime;
 public class CollaborativeTaskCreationResponseModel {
     private final String title;
     private final String id;
-    private final LocalDateTime deadline;
-    private final StudentUser leader;
-
+    final String type = "Collaborative";
     /**
      * A response model for successful Collaborative task creation.
      * @param title - the title of the Collaborative Task
      * @param id - the Collaborative Task's id
-     * @param deadline - the Collaborative Task's deadline
-     * @param leader - the Collaborative Task's leader
      */
-    public CollaborativeTaskCreationResponseModel(String title, String id,  LocalDateTime deadline, StudentUser leader) {
+    public CollaborativeTaskCreationResponseModel(String title, String id) {
         this.title = title;
         this.id = id;
-        this.deadline = deadline;
-        this.leader = leader;
     }
 
     public String getTitle() {
@@ -36,11 +27,7 @@ public class CollaborativeTaskCreationResponseModel {
         return id;
     }
 
-    public LocalDateTime getDeadline() {
-        return deadline;
-    }
-
-    public StudentUser getLeader() {
-        return leader;
+    public String getType() {
+        return type;
     }
 }
